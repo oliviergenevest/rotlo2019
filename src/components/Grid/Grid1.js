a42e9ea0ccb201b8a3a371f2698965156b00b9d0
oliviergenevest <olivier.genevest@gmail.com>
2019-04-13 08:15:44 +0200

    grid edge fix

--- src/components/Grid/Grid1.js
+++ src/components/Grid/Grid1.js
@@ -12,7 +12,19 @@ import styles from './Grid1.module.scss'
 
 const Grid1 = ({image, text1, heroText, text3, flyer}) => (
     <div className= {styles.gridContainer} >
-	    <div className={styles.contentWrapper} >  
+	   
+	     <div className= {styles.flyer} > 
+      		<Fade delay={800}><HeadShake  delay={1500}>{flyer} 	</HeadShake></Fade>
+        </div>	
+       	<div className= {styles.scrollDown} >
+           <Fade down  delay={1000} distance="10px"><ScrollDownAnimation/></Fade>
+        </div>
+        <div className= {styles.image}> 
+      	{image} 
+        </div>			
+		<div className= {styles.bgGradient} > </div>
+		<div className= {styles.bgDots} > </div>
+		 <div className={styles.contentWrapper} >  
 	        <div className= {styles.hero}>
 	          <Fade up  delay={300} distance="10px">
 	           <h1 className= {styles.title}>Festival<br/>{ heroText.text }</h1>
@@ -28,18 +40,7 @@ const Grid1 = ({image, text1, heroText, text3, flyer}) => (
 	       		 	<p>{ text3.text }</p>
 	           	</Fade>
 	       </div>
-	    </div>
-	     <div className= {styles.flyer} > 
-      		<Fade delay={800}><HeadShake  delay={1500}>{flyer} 	</HeadShake></Fade>
-        </div>	
-       	<div className= {styles.scrollDown} >
-           <Fade down  delay={1000} distance="10px"><ScrollDownAnimation/></Fade>
-        </div>
-        <div className= {styles.image}> 
-      	{image} 
-        </div>	
-		<div className= {styles.bgDots} > </div>
-		<div className= {styles.bgGradient} > </div>       
+	    </div>       
     </div>
  
 )
