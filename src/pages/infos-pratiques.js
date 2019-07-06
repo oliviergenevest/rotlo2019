870d8831ce5156c60ab6786630ef809671b68190
oliviergenevest <olivier.genevest@gmail.com>
2019-07-06 14:53:35 +0200

    ajout info billetterie

--- src/pages/infos-pratiques.js
+++ src/pages/infos-pratiques.js
@@ -16,6 +16,9 @@ const InfosPratiques = () => (
     
     <h1>Infos pratiques</h1>
     
+    <h2>Billetterie</h2>
+     <p>La billetterie se fait uniquement sur place.</p>
+     <p>Tarif : 15 € - 12 € (tarif réduit)</p>
     <h2>Se loger</h2>
     <p>Un espace est aménagé sur le site du Pas des Ondes, pour planter vos tentes. Le bivouac y est autorisé le soir du festival.</p>
     <p>Vous trouverez également la liste complète des hébergements (campings, chambres d'hôtes, hôtels, hébergements insolites..) sur le site de l'Office de Tourisme de La Motte Chalancon : <a href="http://www.lamottechalancon-tourisme.com/ou-dormir/language/fr.html">Office de Tourisme de La Motte Chalancon</a></p>
@@ -27,6 +30,9 @@ const InfosPratiques = () => (
     <p>Plan d'eau du Pas des Ondes<br/>
     Adresse : D61, 26510 Cornillon-sur-l'Oule</p>
     <p>Pensez également au covoiturage ! </p>
+
+    <h2>Infos</h2>
+    <p>04 75 27 24 67</p>
 
      <h2>Devenir bénévole</h2>
     <p>prochainement</p>
