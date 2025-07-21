1e27550eddd853ed642a9a1bbf5cb5fc81da24c5
oliviergenevest <olivier.genevest@gmail.com>
2025-07-21 20:19:00 +0200

    add react-player

--- src/components/video.js
+++ src/components/video.js
@@ -1,32 +1,30 @@
 
 import React from 'react'
+import ReactPlayer from 'react-player'
+import styled from 'styled-components'
+
+
+const StyledReactPlayer =  styled.div`
+padding-top: 56.25%;
+  position: relative; 
+  
+`
+const VideoWrapper=  styled.div`
+  width: 100%; 
+  height: auto;
+  padding-top: 56.25%;
+  position: relative; 
+   & > div {
+	  position: absolute; 
+	  height:auto;}
+`
+
+
 const Video = ({ videoSrcURL, videoTitle, ...props }) => (
-  <div 
-	  className="video"
-	  style={{
-	        position: "relative",
-	        paddingBottom: "56.25%" /* 16:9 */,
-	        paddingTop: 25,
-	        height: 0
-	      }}
-  >
-    <iframe
-      src={videoSrcURL}
-      title={videoTitle}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
-      style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%"
-        }}
-      allowFullScreen
-    />
-  </div>
+  
+  
+    <ReactPlayer    style={{ width: '100%', height: 'auto', aspectRatio: '16/9' }} src={videoSrcURL} />
+
 )
 
 export default Video
